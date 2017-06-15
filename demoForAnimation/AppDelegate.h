814b635fcac2723982d73456fa0314c5674d4550
xchenLee <xchen.lee@gmail.com>
2017-06-15 19:07:03 +0800

    Initial Commit

---
+++ demoForAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demoForAnimation
+//
+//  Created by danlan on 2017/6/15.
+//  Copyright © 2017年 lxc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demoForAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demoForAnimation
+//
+//  Created by danlan on 2017/6/15.
+//  Copyright © 2017年 lxc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
