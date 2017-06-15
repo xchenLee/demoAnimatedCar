bd64d51c7cca337331c8b9b10ab3ef309d71cfee
李星辰 <xchen.lee@gmail.com>
2017-06-16 02:06:00 +0800

    add tire animation

--- demoForAnimation/LiveGiftFerrari.m
+++ demoForAnimation/LiveGiftFerrari.m
@@ -39,6 +39,8 @@ static const CGFloat kFerrarih = 329.0;
 
 - (void)play {
     [self doEnterAnimation];
+    [self doTireRotateAnimation];
+    [self doBTireRotateAnimation];
 }
 
 - (void)doEnterAnimation {
@@ -54,6 +56,25 @@ static const CGFloat kFerrarih = 329.0;
     [self.mainFrame.layer addAnimation:animation forKey:@"position"];
 }
 
+
+- (void)doTireRotateAnimation {
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    animation.duration = 0.6;
+    animation.toValue = @(M_PI * 2);
+    animation.repeatCount = MAXFLOAT;
+    animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
+    [self.fTireFrame.layer addAnimation:animation forKey:@"transform.rotation.z"];
+}
+
+- (void)doBTireRotateAnimation {
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    animation.duration = 0.6;
+    animation.toValue = @(M_PI * 2);
+    animation.repeatCount = MAXFLOAT;
+    animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
+    [self.bTireFrame.layer addAnimation:animation forKey:@"transform.rotation.z"];
+}
+
 - (void)construct {
     
     CGRect frame = CGRectMake(0, 0, kScreenWidth, kFerrarih / self.scaleRate);
@@ -65,14 +86,24 @@ static const CGFloat kFerrarih = 329.0;
     _mainFrame.image = [self loadImage:@"car/body"];
     [self addSubview:_mainFrame];
     
-    CGRect tireFrame = CGRectMake(0, 0, [self valueCompat:80], [self valueCompat:80]);
+    CGRect tireFrame = CGRectMake(0, 0, [self valueCompat:68], [self valueCompat:68]);
     tireFrame.origin.x = [self valueCompat:243.0];
-    tireFrame.origin.y = self.frame.size.height - tireFrame.size.height - [self valueCompat:76];
+    tireFrame.origin.y = self.frame.size.height - tireFrame.size.height - [self valueCompat:78];
+    
+    CGRect btireFrame = CGRectMake(0, 0, [self valueCompat:68], [self valueCompat:68]);
+    btireFrame.origin.x = [self valueCompat:572.0];
+    btireFrame.origin.y = self.frame.size.height - tireFrame.size.height - [self valueCompat:90];
+
     
     _fTireFrame = [UIImageView new];
     _fTireFrame.image = [self loadImage:@"car/tire"];
     _fTireFrame.frame = tireFrame;
     [_mainFrame addSubview:_fTireFrame];
+    
+    _bTireFrame = [UIImageView new];
+    _bTireFrame.image = [self loadImage:@"car/tire"];
+    _bTireFrame.frame = btireFrame;
+    [_mainFrame addSubview:_bTireFrame];
 }
 
 - (CGFloat)scaleRate {
